267870310ad999b8e3ee23ce1c4dfe7fd60e21b5
Ben Hardill <hardillb@gmail.com>
2020-07-22 10:09:38 +0100

    Add HTML striping so I can include ep description

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module hardill.me.uk/gopherPodcast
 
 go 1.14
 
-require github.com/mmcdole/gofeed v1.0.0
+require (
+	github.com/grokify/html-strip-tags-go v0.0.0-20200322061010-ea0c1cf2f119 // indirect
+	github.com/mmcdole/gofeed v1.0.0
+)
--- gopherPodcast.go
+++ gopherPodcast.go
@@ -6,8 +6,8 @@ import (
     "fmt"
     "strings"
     "net/http"
-
     "github.com/mmcdole/gofeed"
+    "github.com/grokify/html-strip-tags-go"
 )
 
 func main() {
@@ -30,13 +30,14 @@ func main() {
 	output := feed.Title + "\n\n"
 
 	for _, item := range feed.Items {
-		//fmt.Println(i,item.Title)
 		dirname := strings.ReplaceAll(item.Title, " ", "_")
 		if _, err := os.Stat(dirname); os.IsNotExist(err) {
 			os.Mkdir(dirname, 0755)
 			filename := strings.ReplaceAll(item.Title + ".mp3", " ", "_")
 			downloadFile(item.Enclosures[0].URL, dirname + "/" + filename)
-			output += "9" + item.Title + ".mp3\t" + dirname + "/" + filename + "\n"
+			output += " -- " + item.Title + " --\n\n"
+			output += strip.StripTags(item.Description) + "\n\n"
+			output += "9" + item.Title + ".mp3\t" + dirname + "/" + filename + "\n\n"
 		}
 	}
 
