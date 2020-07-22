8dc6b0cb2f054287f7708812e74a3bd97c6bcef2
Ben Hardill <hardillb@gmail.com>
2020-07-22 21:57:41 +0100

    Add first pass at Blog exporter

--- go.mod
+++ go.mod
@@ -6,4 +6,7 @@ require (
 	github.com/eidolon/wordwrap v0.0.0-20161011182207-e0f54129b8bb // indirect
 	github.com/grokify/html-strip-tags-go v0.0.0-20200322061010-ea0c1cf2f119 // indirect
 	github.com/mmcdole/gofeed v1.0.0
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	jaytaylor.com/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
 )
---
+++ gopherBlog.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+		"os"
+    "fmt"
+    "strings"
+    "io/ioutil"
+    "github.com/mmcdole/gofeed"
+    //"github.com/eidolon/wordwrap"
+    "jaytaylor.com/html2text"
+)
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) == 0 {
+		fmt.Println("You need to pass a feed URL")
+		os.Exit(0)
+	}
+
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(argsWithoutProg[0])
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//wrapper := wordwrap.Wrapper(60, false)
+
+	output := feed.Title + "\n\n"
+
+	for _, item := range feed.Items {
+		dirname := strings.ReplaceAll(item.Title, " ", "_")
+		if _, err := os.Stat(dirname); os.IsNotExist(err) {
+			os.Mkdir(dirname, 0755)
+			filename := strings.ReplaceAll(item.Title, " ", "_")
+			output += " -- " + item.Title + " --\n\n"
+			output += "0" + item.Title + "\t" + dirname + "/" + filename + "\n\n"
+			body, _ := html2text.FromString(item.Content, html2text.Options{PrettyTables: true})
+			writeFile(body, dirname + "/" + filename)
+		}
+	}
+
+	fmt.Println(output)
+}
+
+func writeFile(body string, location string) error {
+	d1 := []byte(body)
+	err := ioutil.WriteFile(location, d1, 0644)
+	return err
+}
